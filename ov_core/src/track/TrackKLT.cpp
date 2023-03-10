1b7c03c63f56bbe01390e3f9b434da7069327dfb
Gustav <gustavtc@gmail.com>
2023-03-10 12:34:08 +0100

    verify the rest

--- ov_core/src/track/TrackKLT.cpp
+++ ov_core/src/track/TrackKLT.cpp
@@ -152,9 +152,33 @@ void TrackKLT::feed_monocular_and_imu(const CameraData &message, std::vector<ov_
   /// Pixel-Aware Gyro-Aided KLT Feature Tracking
   cv::Point3f biasg(0,0,0);
   IMU::Calib imuCalib;
+
+  // FIXME(gustav): this is taken straight from the euroc imucam_chain.yaml
+  float Tbc_data[16] =   {0.0148655429818, -0.999880929698, 0.00414029679422, -0.0216401454975,
+     0.999557249008, 0.0149672133247, 0.025715529948, -0.064676986768,
+    -0.0257744366974, 0.00375618835797, 0.999660727178, 0.00981073058949,
+     0.0, 0.0, 0.0, 1.0};
+  imuCalib.Tbc = cv::Mat(4, 4, CV_32F, Tbc_data);
+
   std::vector<IMU::Point> vImuFromLastFrame;
-  std::vector<cv::KeyPoint> empty_keypoint;
-  cv::Mat empty_mat;
+  auto it = imu_data.begin();
+  while (it != imu_data.end() && it->timestamp < message.timestamp)
+  {
+    if (it->timestamp < timestamp_last[cam_id]){
+      imu_data.erase(it);
+    }
+    IMU::Point imu;
+    imu.t   = it->timestamp;
+    imu.a.x = it->am.x();
+    imu.a.y = it->am.y();
+    imu.a.z = it->am.z();
+    imu.w.x = it->wm.x();
+    imu.w.y = it->wm.y();
+    imu.w.z = it->wm.z();
+    vImuFromLastFrame.push_back(imu);
+    it++;
+  }
+
   auto K = camera_calib.at(cam_id)->get_K();
   auto D = camera_calib.at(cam_id)->get_D();
   auto K_mat_double = cv::Mat(3,3,CV_64F, K.val);
@@ -196,12 +220,13 @@ void TrackKLT::feed_monocular_and_imu(const CameraData &message, std::vector<ov_
   //                   cv::Mat K_, cv::Mat DistCoef_, const cv::Mat &normalizeTable_,
   //                   GyroAidedTracker::eType predictMethod_ = GyroAidedTracker::OPENCV_OPTICAL_FLOW_PYR_LK);
 
-  GyroAidedTracker gyroPredictMatcher(0, 0, img_last[cam_id], img,
-                    pts_left_old, empty_keypoint,
-                    kpts0_un, empty_keypoint,
-                    vImuFromLastFrame, biasg,
-                    K_mat, D_mat, empty_mat,
-                    GyroAidedTracker::OPENCV_OPTICAL_FLOW_PYR_LK);
+  GyroAidedTracker gyroPredictMatcher(message.timestamp, timestamp_last[cam_id], img_last[cam_id], img,
+                    pts_left_old, std::vector<cv::KeyPoint>(),
+                    kpts0_un, std::vector<cv::KeyPoint>(),
+                    vImuFromLastFrame, imuCalib, biasg,
+                    K_mat, D_mat, cv::Mat(),
+                    GyroAidedTracker::OPENCV_OPTICAL_FLOW_PYR_LK,
+                    GyroAidedTracker::PIXEL_AWARE_PREDICTION);
 
   gyroPredictMatcher.TrackFeatures();
   // setFrameWithoutGeometryValid(curFrame, gyroPredictMatcher); // save temporal states
